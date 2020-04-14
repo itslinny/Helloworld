c9b035d7dfbe75c3c0e63d7ddbeb530925e8d7aa
Linny Poovakkattu <lpoovakk@in.ibm.com>
2020-04-14 18:26:56 +0530

    Update with game file

---
+++ Helloworld/src/Game.java
@@ -0,0 +1,13 @@
+
+public class Game {
+String gamename;
+
+public String getGamename() {
+	return gamename;
+}
+
+public void setGamename(String gamename) {
+	this.gamename = gamename;
+}
+
+}
--- Helloworld/src/HelloGit.java
+++ Helloworld/src/HelloGit.java
@@ -2,5 +2,8 @@
 public class HelloGit {
 public static void main(String[] args) {
 	System.out.println("Hello from git");
+	//Adding gAME
+	Game g = new Game();
+	g.setGamename("Golf");
 }
 }
